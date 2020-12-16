c1968c6fb39d2f7ced9fdcdf50ffd1ca80317ea6
Sicong Tang <scalliontang@gmail.com>
2020-12-15 19:25:40 -0500

    finished log in

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "../styles/App.css"
+import {TOKEN_KEY} from "../constants";
+import TopBar from "./TopBar";
+import Main from "./Main";
+
+function App() {
+  const [ isLoggedIn, setIsLoggedIn ] = useState(
+      localStorage.getItem(TOKEN_KEY) ? true : false
+  );
+
+  const logout = () => {
+    console.log("log out");
+    localStorage.removeItem(TOKEN_KEY);
+    setIsLoggedIn(false);
+  }
+
+  const loggedIn = (token) => {
+    if (token) {
+      localStorage.setItem(TOKEN_KEY, token);
+      setIsLoggedIn(true);
+    }
+  };
+  return (
+    <div className="App">
+      <TopBar isLoggedIn={isLoggedIn} handleLogout={logout} />
+      <Main isLoggedIN={isLoggedIn} handleLoggedIn={loggedIn}/>
+    </div>
+  );
+}
+
+export default App;
